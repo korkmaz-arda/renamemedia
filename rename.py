3ae9f797a34501a55c6dc063c8b8a1deb7ff642c
korkmaz-arda <arda.korkmaz1996@gmail.com>
2024-11-17 23:53:56 +0300

    Extend support for MP4, AIFF, Ogg, Ogg Opus and Window Media Audio formats.

--- rename.py
+++ rename.py
@@ -2,12 +2,15 @@ import os
 from mutagen.mp3 import MP3
 from mutagen.easyid3 import EasyID3
 from mutagen.flac import FLAC
+from mutagen.aiff import AIFF
+from mutagen.ogg import OggFileType
+from mutagen.asf import ASF
+from mutagen.mp4 import MP4
 
 
-def rename_media_files(media_dir, accepted_formats=['mp3', 'flac']):
+def rename_media_files(media_dir, accepted_formats=['mp3', 'mp4', 'flac', 'aiff', 'ogg', 'opus', 'wma']):
     for filename in os.listdir(media_dir):
         full_path = os.path.join(media_dir, filename)
-
         file_extension = filename.split('.')[-1].lower()
 
         if file_extension not in accepted_formats:
@@ -16,8 +19,16 @@ def rename_media_files(media_dir, accepted_formats=['mp3', 'flac']):
         try:
             if file_extension == "mp3":
                 audio = MP3(full_path, ID3=EasyID3)
+            elif file_extension == "mp4":
+                audio = MP4(full_path)
             elif file_extension == "flac":
                 audio = FLAC(full_path)
+            elif file_extension == "aiff":
+                audio = AIFF(full_path)
+            elif file_extension in ["ogg", "opus"]:
+                audio = OggFileType(full_path)
+            elif file_extension == "wma":
+                audio = ASF(full_path)
             else:
                 print(f"Skipping unsupported file format '{filename}'")
                 continue
@@ -34,7 +45,6 @@ def rename_media_files(media_dir, accepted_formats=['mp3', 'flac']):
 
                 os.rename(full_path, new_path)
                 print(f"Renamed '{filename}' to '{sanitized_title}.{file_extension}'")
-
             else:
                 print(f"No title tag found for '{filename}', skipping.")
 
