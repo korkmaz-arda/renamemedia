d92888ca6b754330ad617f52cf64a34d14c8ccad
korkmaz-arda <arda.korkmaz1996@gmail.com>
2024-12-03 13:08:57 +0300

    Add: setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="renamemedia",
+    version="1.0.0",
+    description="Rename media files based on their metadata.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Arda Korkmaz",
+    url="https://github.com/korkmaz-arda/korkmaz-arda",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "mutagen>=1.45",
+    ],
+    entry_points={
+        "console_scripts": [
+            "renamemedia=renamemedia.rename:rename_media_files",
+        ],
+    },
+    python_requires=">=3.6",
+)
