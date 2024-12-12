375cdf59b4fafde84e0ce526c189e5f682248127
Arda Korkmaz <109353825+korkmaz-arda@users.noreply.github.com>
2024-12-12 23:34:06 +0300

    Add classifiers

--- setup.py
+++ setup.py
@@ -18,5 +18,10 @@ setup(
             "renamemedia=renamemedia.rename:rename_media_files",
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires=">=3.6",
 )
