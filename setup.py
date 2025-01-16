14d0a9a5c3ad2244f62ab3051df72a569a8d5959
Arda Korkmaz <109353825+korkmaz-arda@users.noreply.github.com>
2025-01-15 23:42:25 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,10 +20,6 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
